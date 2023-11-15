fdbbeea6b9ed08b0cd690109c9fd6cb5dfed7357
Ilan Nestor Angeles Rodriguez <ilananagelesrodriguez@gmail.com>
2023-11-15 11:41:28 -0500

    chore: reorganizar carpetas del pseudocodigo

--- Python/OPERADORES/Basico/Ejercicio_5.py
+++
@@ -1,6 +0,0 @@
-import math
-lado=5
-area=math.pow(lado,2)*math.sqrt(3)
-print("El área del tetraedro de lado: ",lado," es: ",area)
-volumen=(math.pow(lado,3)*math.sqrt(2))/12
-print("El volumen del tetraedro de lado: ",lado," es: ",volumen)
