020205cc3b2ab33c42d5d70c8adc5333071b5eb5
NiltonRamosE <150812910+NiltonRamosE@users.noreply.github.com>
2023-11-17 09:49:11 -0500

    Update Ejercicio_04.php

--- PHP/ESTRUCTURA_BUCLES/FOR/Avanzado/Ejercicio_04.php
+++ PHP/ESTRUCTURA_BUCLES/FOR/Avanzado/Ejercicio_04.php
@@ -2,24 +2,22 @@
     $suma = 0;
     $numeros = [7, 12, 5, 8, 3];
     
-    for ($i = 0; $i < 5; $i++) {
-        if (esPrimo($numeros[$i])) {
-            $suma += $numeros[$i];
-        }
-    }
-    
-    echo "La suma de los números primos es: ".$suma;
-    
-    // Función para verificar si un número es primo
-    function esPrimo($numero) {
-        if ($numero <= 1) {
-            return false;
-        }
-        for ($i = 2; $i <= sqrt($numero); $i++) {
-            if ($numero % $i == 0) {
-                return false;
+    foreach ($numeros as $numero) {
+        $esPrimo = true;
+        if ($numero > 1) {
+            for ($i = 2; $i <= sqrt($numero); $i++) {
+                if ($numero % $i == 0) {
+                    $esPrimo = false;
+                    break;
+                }
             }
+            if ($esPrimo) {
+                $suma += $numero;
+            }
+        }else{
+            $esPrimo = false;
         }
-        return true;
     }
+    
+    echo "La suma de los números primos es: " . $suma;
 ?>
